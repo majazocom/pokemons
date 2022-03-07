9b2da6c3ea5f8e63b4203e0e6b9c1d5509f9d850
majazocom <maja.gedda@zocom.se>
2022-03-07 12:06:52 +0100

    pokemonform component added

---
+++ src/Components/PokemonForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function PokemonForm() {
+  return (
+    <article>
+        <h4>Add new Pokemon</h4>
+        <input type="number"></input>
+        <input type="text"></input>
+        <input type="checkbox" defaultChecked="false"></input>
+    </article>
+  )
+}
