441dd9cb3c58e4378cf49170a35d54c275da7d4f
majazocom <maja.gedda@zocom.se>
2022-03-07 11:34:50 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import PokemonCard from './Components/PokemonCard';
 
 function App() {
@@ -11,11 +11,42 @@ function App() {
     { id: 5, name: "Charmeleon", caught: false },
     { id: 6, name: "Charizard", caught: false }
   ]);
+  const [pokeCount, setPokeCount] = useState(0);
+
+  function pokemonCaught(id) {
+    //kör vi en kopia på statet
+    let pokemonList = [...pokemons];
+    //söker vi efter objektet i listan som har id'et som vi fårr från barnkomponenten
+    let currentPokemon = pokemonList.find((pokemon) => pokemon.id === id );
+    //går in i det objektet och ändrar värdet på propertyn caugth till motsatta värdet
+    currentPokemon.caught = !currentPokemon.caught;
+    //letar efter index på objekt i listan som matchar argumentet vi tagit in i funktionen
+    let currId = pokemonList.findIndex((pokemon) => pokemon.id === id);
+    //splicear mha id'et vi matchat, tar bort det gamla objektet och ersätter med vårt ändrade objekt
+    pokemonList.splice(currId, 1, currentPokemon);
+    //kallar på setPokemons som uppdaterar statet
+    setPokemons(pokemonList);
+  }
+
+  function updatePokeCount() {
+    //skapar en kopia på pokemons
+    let caughtPokemons = [...pokemons];
+    //filtrerar ut alla objekt som har värdet true på propertyn caught
+    caughtPokemons = caughtPokemons.filter((pokemon) => pokemon.caught === true);
+    console.log(caughtPokemons);
+    //sätter statet pokeCount till längden på listan
+    setPokeCount(caughtPokemons.length);
+  }
+
+  //useEffect som tar funktionen som skall köras som första parameter och vad som triggar den som andra
+  useEffect(updatePokeCount, [pokemons]);
+
   return (
     <div className="App">
+      <h2>Pokedex: ({pokeCount} / 151)</h2>
       {
         pokemons.map((pokemon) => (
-          <PokemonCard pokemon={pokemon} key={pokemon.id} />
+          <PokemonCard pokemonCaught={pokemonCaught} pokemon={pokemon} key={pokemon.id} />
         ))
       }
     </div>
--- src/Components/PokemonCard.jsx
+++ src/Components/PokemonCard.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
-export default function PokemonCard({ pokemon: { name, caught } }) {
+export default function PokemonCard(props) {
+    function toggleChecked() {
+        props.pokemonCaught(props.pokemon.id);
+    }
     return (
         <article>
-            <input type="checkbox" checked={caught}></input>
-            <h4>{name}</h4>
+            <input type="checkbox" onChange={toggleChecked} checked={props.pokemon.caught}></input>
+            <h4>{props.pokemon.name}</h4>
         </article>
     )
 }
